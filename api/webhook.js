980e983b7fbe47cf23e38a24ad794eb39fd2c55a
Kirill Beznisko <kyrylo.beznisko@shkolo.com>
2023-07-14 20:36:52 +0300

    Fixes

--- api/webhook.js
+++ api/webhook.js
@@ -1,18 +1,5 @@
-import {Telegraf} from 'telegraf';
-import 'dotenv/config'
-import remove from "../src/remove.js";
+import App from "../index.js";
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
-
-bot.use(remove);
-
-bot.start((ctx) => ctx.reply('Welcome!'));
-
-bot.launch().then(() => {
-    console.log('Bot start!')
-});
-
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
-
-export default bot;
+export default (req, res) => {
+    App();
+}
