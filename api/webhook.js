3ffdf696aa14ac30fe8a8fc6e2bb4971208be7f0
Kirill Beznisko <kyrylo.beznisko@shkolo.com>
2023-07-14 20:14:37 +0300

    Add webhook file for vercel

---
+++ api/webhook.js
@@ -0,0 +1,16 @@
+import {Telegraf} from 'telegraf';
+import 'dotenv/config'
+import remove from "../src/remove.js";
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.use(remove);
+
+bot.start((ctx) => ctx.reply('Welcome!'));
+
+bot.launch().then(() => {
+    console.log('Bot start!')
+});
+
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
